0dde90befc2ce23eac551c1876a690325bdeb3b9
jin <kpu9939@gmail.com>
2022-02-13 10:10:39 +0000

    views

--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from .models import Photo
 # Create your views here.
+
+def photo_list(request):
+    photos = Photo.objects.all()
+    return render(request,'photo/list.html',{'photos':photos})
+    
